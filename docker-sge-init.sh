d480cddf2fda512886afe065134e3b2533fbb307
Steve Moss <gawbul@gmail.com>
2014-12-12 21:23:35 +0000

    Removed hardcoded hostname from init script
    Removed hardcoded hostname left in the docker-sge-init.sh script by accident.

--- docker-sge-init.sh
+++ docker-sge-init.sh
@@ -3,12 +3,12 @@
 # get stored SGE hostname
 export SGE_HOST=`cat /opt/sge/default/common/act_qmaster`
 
-# rename files
-find /opt/sge/ -name "*$SGE_HOST*" | sed -e "p;s/f7e33910641b/$HOSTNAME/" | xargs -n2 mv
+# rename files from SGE_HOST to HOSTNAME
+find /opt/sge/ -name "*$SGE_HOST*" | sed -e "p;s/$SGE_HOST/$HOSTNAME/" | xargs -n2 mv
 
 # replace SGE_HOST text in files
 grep -Rl "$SGE_HOST" /opt/sge/ | xargs sed -i "s/$SGE_HOST/$HOSTNAME/g"
 
-# execute SGE
+# restart SGE
 /etc/init.d/sgemaster.docker-sge restart
 /etc/init.d/sgeexecd.docker-sge restart
